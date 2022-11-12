effd07bb534ed94ff2e6835830e9f9e03aa8e9bd
earyf <76503622+earyf@users.noreply.github.com>
2022-11-12 02:22:52 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -54,8 +54,64 @@ $config['timeZone'] =  "Asia/Kolkata";
 */
 $config['db']['hostname'] =  "localhost";
 $config['db']['username'] =  "root";
-$config['db']['password'] =  "";
-$config['db']['database'] =  "checkerbot";
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Bot Token
+|--------------------------------------------------------------------------
+|
+| Change this to your Bot API Token
+| It can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['botToken'] = "5686632028:AAHLA-9yMJVm9Ilvr-ER6Q6QmomTdhUocjM";
+
+/*
+|--------------------------------------------------------------------------
+| Admin User ID
+|--------------------------------------------------------------------------
+|
+| Change this to Admin's Numeric User ID
+| ID can be obtained from https://telegram.dog/username_to_id_bot
+|
+*/
+$config['adminID'] = "1679669329";
+
+/*
+|--------------------------------------------------------------------------
+| Logs Channel ID
+|--------------------------------------------------------------------------
+|
+| Create a New Channel/Group for logging data
+| ID can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['logsID'] =  "5725754088";
+
+/*
+|--------------------------------------------------------------------------
+| Timezone
+|--------------------------------------------------------------------------
+|
+| Current timezone for Logging Activities with time
+| It can be obtained from http://1min.in/content/international/time-zones
+| By Default it's in IST
+|
+*/
+$config['timeZone'] =  "Asia/Kolkata";
+
+/*
+|--------------------------------------------------------------------------
+| Database
+|--------------------------------------------------------------------------
+| Database to Store User Data
+|
+*/
+$config['db']['hostname'] =  "sql9.freesqldatabase.com";
+$config['db']['username'] =  "sql9562582";
+$config['db']['password'] =  "kuUacRAsSj";
+$config['db']['database'] =  "sql9562582";
 
 /*
 |--------------------------------------------------------------------------
@@ -78,5 +134,5 @@ $config['anti_spam_timer'] =  "20";
 | array('sk1','sk2','sk3')
 |
 */
-$config['sk_keys'] =  array('sk_live_69GKI0saLB8uIEnxzv8VTvRX');
+$config['sk_keys'] =  array('sk_live_69GKI0saLB8uIEnxzv8VTvRX','sk_live_Vvh4jrNVcdhR87kJcBTWOjXq00AT7SIOSd',');
 ?>
